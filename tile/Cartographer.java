ed58d7f098a68bdecd66a8079c4b9e6001279ede
rylucs17 <ryluchs17@saintjosephhs.com>
2016-04-21 09:39:04 -0400

    began migrating map generating functions into their own class called Cartographer

---
+++ tile/Cartographer.java
@@ -0,0 +1,191 @@
+/**
+ * A map generator for RogueFrame. Credit to the people at Rogue Basin for some concepts and code samples
+ * @author Ryan Luchs
+ */
+package tile;
+
+import java.util.Random;
+
+/**
+ * @author SJHSStudent
+ *
+ */
+public class Cartographer {
+
+	public static long seed = 0;
+	
+	// defaults
+	public static final short TILE_DEFAULT_SPACE = 0;
+	public static final short TILE_DEFAULT_WALL = 1;
+	
+	// other tiles
+	public static final short TILE_SOIL = 2;
+	public static final short TILE_WATER = 3;
+	public static final short TILE_MAGMA = 4;
+	public static final short TILE_SPIKE = 5;
+	
+	public static AbstractTile makeTile(short id, int x, int y) {
+		AbstractTile tile;
+		
+		switch(id) {
+		
+			case TILE_DEFAULT_SPACE:
+				break;
+				
+			case TILE_DEFAULT_WALL:
+				break;
+				
+			case TILE_SOIL:
+				tile = new Soil(x, y);
+				break;
+				
+			case TILE_WATER:
+			case TILE_MAGMA:
+			case TILE_SPIKE:
+		}
+	}
+	
+	public static AbstractTile[][] createMapFromBooleanArray(boolean[][] array) {
+		AbstractTile[][] map = new AbstractTile[array.length][array[0].length];
+		
+		for(int y = 0; y < array[0].length; y++) {
+			for(int x = 0; x < array.length; x++) {
+				map[x][y] = array[x][y] ? new Spike(x, y) : new Soil(x, y);
+			}
+		}
+		
+		return map;
+	}
+	
+	public static boolean[][] cellularAutomata(int columns, int rows) {
+		
+		// the final map
+		boolean[][] map = new boolean[columns][rows];
+		
+		// map used for updates
+		boolean[][] update = new boolean[columns][rows];
+		
+		// random number generator
+		Random r = new Random(seed);
+		
+		for(int x = 0, y = 0; y < rows; y++) {
+			for(x = 0; x < columns; x++) {
+				if(x == 0) {
+					map[x][y] = true;
+				} else if(y == 0) {
+					map[x][y] = true;
+				} else if(x == columns - 1) {
+					map[x][y] = true;
+				} else if(y == rows - 1) {
+					map[x][y] = true;
+				} else {
+					
+					map[x][y] = r.nextInt(100) < 40;
+					
+//					middle = columns / 2;
+//					
+//					if(y == middle) {
+//						plane[x][y] = false;
+//					} else {
+//						plane[x][y] = generate.nextInt(100) < 40;
+//					}
+				}
+			}
+		}
+	
+		print(map);
+		
+		for(int i = 0; i < 4; i++) {
+			for(int x = 1, y = 1; y < rows - 1; y++) {
+				for(x = 1; x < columns - 1; x++) {
+					update[x][y] = placeWall(map, x, y);
+				}
+			}
+		
+			print(update);
+			
+			for(int x = 1, y = 1; y < rows - 1; y++) {
+				for(x = 1; x < columns - 1; x++) {
+					map[x][y] = update[x][y];
+				}
+			}
+		}
+
+		return map;
+	}
+	
+	private static int getAdjacentWalls(boolean[][] m, int x, int y) {
+		int iX = x - 1;
+		int iY = y - 1;
+	
+		int counter = 0;
+	
+		for(iY = y - 1; iY <= y + 1; iY++) {
+			for(iX = x - 1; iX <= x + 1; iX++) {
+				if(!(iX == x && iY == y)) {
+					if(isWall(m, iX, iY)) {
+						counter += 1;
+					}
+				}
+			}
+		}
+		return counter;
+	}
+	
+	private static boolean isWall(boolean[][] m, int x,int y) {
+		// Consider out-of-bound a wall
+		if(isOutOfBounds(x, y, m.length, m[1].length)) {
+			return true;
+		}
+	
+		if(m[x][y] == true) {
+			return true;
+		}
+	
+		if(m[x][y] == false) {
+			return false;
+		}
+		return false;
+	}
+	
+	private static boolean isOutOfBounds(int x, int y, int columns, int rows) {
+		if(x < 0 || y < 0) {
+			return true;
+		}
+		else if(x > columns - 1 || y > rows - 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean placeWall(boolean[][] m, int x, int y) {
+		int neighbors = getAdjacentWalls(m, x, y);
+		
+		if(m[x][y] == true) {
+			if(neighbors >= 4) {
+				return true;
+			}
+			if(neighbors < 2) {
+				return false;
+			}
+		} else {
+			if(neighbors >= 5) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	private static void print(boolean[][] m) {
+		for(int y = 0, x = 0; y < m[0].length; y++) {
+			for(x = 0; x < m.length; x++) {
+				System.out.print(m[x][y] ? "#" : "-");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		System.out.print("\n");
+	}
+		
+}
--- tile/TileMap.java
+++ tile/TileMap.java
@@ -41,9 +41,10 @@ public class TileMap {
 
 		this.rows = rows; this.columns = columns;
 		
-		tiles = new AbstractTile[this.columns][this.rows]; 
+		Cartographer.seed = generate.nextLong();
+		tiles = Cartographer.createMapFromBooleanArray(Cartographer.cellularAutomata(columns, rows)); 
 
-		cellularAutomata();
+		//cellularAutomata();
 		
 		AbstractEntity e;
 		entities = new ArrayList<AbstractEntity>();
@@ -68,9 +69,8 @@ public class TileMap {
 
 		this.rows = rows; this.columns = columns;
 		
-		tiles = new AbstractTile[this.columns][this.rows];
-		
-		cellularAutomata();
+		Cartographer.seed = seed;
+		tiles = Cartographer.createMapFromBooleanArray(Cartographer.cellularAutomata(columns, rows));
 
 //		for(int y = 0; y < this.rows; y++) {
 //			for(int x = 0; x < this.columns; x++) {
@@ -90,150 +90,6 @@ public class TileMap {
 			e.setMap(this);
 			entities.add(e);
 		}
-	}
-	
-	private void cellularAutomata() {
-		boolean[][] plane = new boolean[columns][rows];
-		boolean[][] update = new boolean[columns][rows];
-		
-		//int middle = 0;
-		for(int y = 0; y < this.rows; y++) {
-			for(int x = 0; x < this.columns; x++) {
-				if(x == 0) {
-					plane[x][y] = true;
-				} else if(y == 0) {
-					plane[x][y] = true;
-				} else if(x == length() - 1) {
-					plane[x][y] = true;
-				} else if(y == height() - 1) {
-					plane[x][y] = true;
-				} else {
-					
-					plane[x][y] = generate.nextInt(100) < 40;
-					
-//					middle = columns / 2;
-//					
-//					if(y == middle) {
-//						plane[x][y] = false;
-//					} else {
-//						plane[x][y] = generate.nextInt(100) < 40;
-//					}
-				}
-			}
-		}
-		
-		print(plane);
-		
-		for(int i = 0; i < 4; i++) {
-			for(int x = 1, y = 1; y < rows - 1; y++) {
-				for(x = 1; x < columns - 1; x++) {
-					update[x][y] = placeWall(plane, x, y);
-				}
-			}
-		
-			print(update);
-			
-			for(int x = 1, y = 1; y < rows - 1; y++) {
-				for(x = 1; x < columns - 1; x++) {
-					plane[x][y] = update[x][y];
-				}
-			}
-		}
-		
-		for(int y = 0; y < this.rows; y++) {
-			for(int x = 0; x < this.columns; x++) {
-				tiles[x][y] = plane[x][y] ? new Spike(x, y) : new Soil(x, y);
-			}
-		}
-	}
-	
-	private int getAdjacentWalls(boolean[][] m, int x, int y) {
-		int startX = x - 1;
-		int startY = y - 1;
-		int endX = x + 1;
-		int endY = y + 1;
- 
-		int iX = startX;
-		int iY = startY;
- 
-		int wallCounter = 0;
- 
-		for(iY = startY; iY <= endY; iY++) {
-			for(iX = startX; iX <= endX; iX++)
-			{
-				if(!(iX==x && iY==y))
-				{
-					if(isWall(m, iX, iY))
-					{
-						wallCounter += 1;
-					}
-				}
-			}
-		}
-		return wallCounter;
-	}
-	
-	private boolean isWall(boolean[][] m, int x,int y)
-	{
-		// Consider out-of-bound a wall
-		if( IsOutOfBounds(x,y) )
-		{
-			return true;
-		}
- 
-		if( m[x][y] == true )
-		{
-			return true;
-		}
- 
-		if( m[x][y] == false )
-		{
-			return false;
-		}
-		return false;
-	}
- 
-	private boolean IsOutOfBounds(int x, int y)
-	{
-		if( x<0 || y<0 )
-		{
-			return true;
-		}
-		else if( x>columns - 1 || y>rows - 1 )
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	private boolean placeWall(boolean[][] m, int x, int y) {
-		int neighbors = getAdjacentWalls(m, x, y);
-		
-		if(m[x][y] == true) {
-			if(neighbors >= 4) {
-				return true;
-			}
-			if(neighbors < 2) {
-				return false;
-			}
-		} else {
-			if(neighbors >= 5) {
-				return true;
-			}
-		}
-		return false;	
-	}
-	
-	private void print(boolean[][] m) {
-		for(int y = 0, x = 0; y < rows; y++) {
-			for(x = 0; x < columns; x++) {
-				System.out.print(m[x][y] ? "#" : "-");
-			}
-			
-			System.out.print("\n");
-		}
-		
-		System.out.print("\n");
 	}
 	
 	/**
